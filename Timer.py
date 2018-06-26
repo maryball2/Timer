44000993c5915d4c28dc987716533c876e9a6a4e
Riley Carpenter <31159790+rileyball@users.noreply.github.com>
2018-06-26 10:29:33 -0400

    Added Message System

--- Timer.py
+++ Timer.py
@@ -8,6 +8,7 @@ import os
 import glob
 import sys
 import random
+global message
 
 if sys.platform == "linux" or sys.platform == "posix":
     clearorcls = "clear"
@@ -20,7 +21,7 @@ total_seconds = 0
 MINUTE_SECONDS = 60
 HOUR_SECONDS = 60*60
 DAY_SECONDS = 24*60*60
-
+message = ""
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # THESE ARE THE PATHS TO MY MUSIC DIRECTORY CHANGE THESE TO YOUR DIRECTOR LEAVE THE /*mp3* AND /*wav* SO IT WORKS
@@ -139,9 +140,17 @@ def minusseconds():
     howmuchtosubtract = int(input("How much do you want to subtract? "))
     total_seconds -= howmuchtosubtract
 
+def messagemake():
+    global message
+    message = input("What would you like your custom message to say? ")
+    clear_screen()
+    print("Message Saved")
+    input()
+    clear_screen()
 
 while whattodonext != "done":
-    print("Type the unit to modify, check, or done ")
+    clear_screen()
+    print("Type the unit to modify, check, write a message, or done ")
     whattodonext = input("What do you want to do? ")
     if whattodonext == "days" or whattodonext == "Days" or whattodonext == "day" or whattodonext == "Day":
         clear_screen()
@@ -182,6 +191,9 @@ while whattodonext != "done":
         print(formatted_time)
         input("Press enter to put in a new number")
         clear_screen()
+    elif whattodonext == "Message" or whattodonext == "message" or whattodonext == "Write a message" or whattodonext == "write a message" or whattodonext == "Write A Message" or whattodonext == "writeamessage":
+        clear_screen()
+        messagemake()
     else:
         while total_seconds > 0:
             clear_screen()
@@ -191,6 +203,12 @@ while whattodonext != "done":
             total_seconds -= 1
             time.sleep(1)
         else:
+            clear_screen()
             song = random.choice(optionalsongs)
             playsound(song)
-            input("Press enter to end this song")
+            print("Press enter to end this song")
+            if message != "":
+                print("THERE IS A MESSAGE FOR YOU:")
+                print(message)
+            input(" ")
+            message = ""
